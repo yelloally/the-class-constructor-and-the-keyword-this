2fd5ed6e1c06c8cf941a3451cfb623475f2cb1e7
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-13 11:56:24 +0100

    Update Program.cs

--- constructor_this/Program.cs
+++ constructor_this/Program.cs
@@ -1,19 +1,18 @@
-﻿using System;
+using System;
 
-namespace MiniGame
+namespace thisMiniGame
 {
-    // Class to store player information
+    //class to store player information
     class Person
     {
-        // Properties of the Person class
+        //properties
         public string Name { get; set; }
         public int Age { get; set; }
         public string Address { get; set; }
 
-        // Constructor for the Person class
         public Person(string name, int age, string address)
         {
-            // Assign the constructor parameters to the class properties using the "this" keyword
+            //assign the constructor parameters to the class properties 
             this.Name = name;
             this.Age = age;
             this.Address = address;
@@ -24,33 +23,33 @@ namespace MiniGame
     {
         static void Main(string[] args)
         {
-            // Welcome message
+            //welcome message
             Console.WriteLine("Welcome to the Mini-Game!");
 
-            // Get the player's name
+            //player's name
             Console.WriteLine("Enter your name: ");
             string name = Console.ReadLine();
 
-            // Get the player's age
+            //player's age
             Console.WriteLine("Enter your age: ");
             int age = int.Parse(Console.ReadLine());
 
-            // Get the player's address
+            //player's address
             Console.WriteLine("Enter your address: ");
             string address = Console.ReadLine();
 
-            // Create a new Person object with the player's information
+            //create a new Person object with the player's information
             Person player = new Person(name, age, address);
 
-            // Start the guessing game
+            //start the guessing game
             Console.WriteLine("Hi " + player.Name + "! Let's play a guessing game.");
             Console.WriteLine("Guess a number between 1 and 10: ");
             int guess = int.Parse(Console.ReadLine());
 
-            // Generate a random number between 1 and 10
+            //generate a random number between 1 and 10
             int number = new Random().Next(1, 11);
 
-            // Check if the player's guess is correct
+            //check if the player's guess is correct
             if (guess == number)
             {
                 Console.WriteLine("Congratulations! You guessed correctly.");
